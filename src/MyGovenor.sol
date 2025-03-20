c02afa0c73d3b01b5884b8c656c73497c297dc57
Bonkesam <bonkesam9@gmail.com>
2025-03-20 11:07:55 +0300

    Adds the timelock controller

--- src/MyGovenor.sol
+++ src/MyGovenor.sol
@@ -11,7 +11,7 @@ import {GovernorVotesQuorumFraction} from "@openzeppelin/contracts/governance/ex
 import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
-contract Governor is
+contract MyGovernor is
     Governor,
     GovernorSettings,
     GovernorCountingSimple,
